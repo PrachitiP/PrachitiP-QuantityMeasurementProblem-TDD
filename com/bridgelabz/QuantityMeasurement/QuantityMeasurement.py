dd0a520e07e03dd094cfc57fb18d7cc507c5a0ab
PrachitiP <patilprachiti35@gmail.com>
2020-12-09 23:58:01 +0530

    Feet to Inch Conversion

--- com/bridgelabz/QuantityMeasurement/QuantityMeasurement.py
+++ com/bridgelabz/QuantityMeasurement/QuantityMeasurement.py
@@ -1,1 +1,24 @@
-print("Quantity Measurement Problem Using TDD")
+import enum
+
+class Feet:
+    def __init__(self, feet):
+        self.feet = feet
+
+    def __eq__(self, other):
+        if isinstance(other, Feet):
+            return self.feet == other.feet
+        elif isinstance(other, Inch):
+            return self.feet == other.inch/12
+        return False
+
+class Inch:
+    def __init__(self, inch):
+        self.inch = inch
+
+    def __eq__(self, other):
+        if isinstance(other, Inch):
+            return self.inch == other.inch
+        elif isinstance(other, Feet):
+            return self.inch == other.feet * 12
+        return False
+
