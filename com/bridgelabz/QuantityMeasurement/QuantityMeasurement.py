9bb9fb9258e9dfe0a07d27151f3e4da5264fc20a
PrachitiP <patilprachiti35@gmail.com>
2020-12-11 18:03:38 +0530

    Compare Feet and Yard

--- com/bridgelabz/QuantityMeasurement/QuantityMeasurement.py
+++ com/bridgelabz/QuantityMeasurement/QuantityMeasurement.py
@@ -22,3 +22,27 @@ class Inch:
             return self.inch == other.feet * 12
         return False
 
+class QuantityMeasurement:
+    def __init__(self, unit, value):
+        self.unit = unit
+        self.value = value
+
+    def compare(self, other):
+        if isinstance(self.unit, Lengths) and isinstance(other.unit, Lengths):
+            if Lengths.convert(self.unit, self.value) == Lengths.convert(other.unit, other.value):
+                return True
+        return False
+
+
+
+class Lengths(enum.Enum):
+    FEET = 12.0
+    INCH = 1.0
+    YARD = 36.0
+    CENTIMETER = 0.4
+
+    def __init__(self, unit):
+        self.unit = unit
+
+    def convert(self, value):
+        return self.unit * value
